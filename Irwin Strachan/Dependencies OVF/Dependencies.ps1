0ac99a69cc621eb4e3037000f619b1af282ac063
Irwin Strachan <irwins@users.noreply.github.com>
2018-04-23 07:59:49 +0200

    Uploaded Dependencies OVF Code

---
+++ Irwin Strachan/Dependencies OVF/Dependencies.ps1
@@ -0,0 +1,176 @@
+﻿<#
+
+        Author: I.C.A. Strachan
+        Version:
+        Version History:
+
+        Purpose: Infrastructure Dependencies script to create AzureADUser from XML file
+
+#>
+
+[CmdletBinding(SupportsShouldProcess=$True)]
+
+Param(
+    [string]
+    $xmlFileName = 'PSConfEU2018.xml',
+
+    [string]
+    $xmlFilePath = 'source\xml'
+)
+
+#Get XML File
+$xmlFile = $(Join-Path -Path "$PSScriptRoot\$xmlFilePath" -ChildPath $xmlFileName)
+
+function ConvertTo-HashTable{
+    param(
+        [PSObject]$PSObject
+    )
+    $splat = @{}
+
+    $PSObject | 
+    Get-Member -MemberType *Property |
+    ForEach-Object{
+         $splat.$($_.Name) = $PSObject.$($_.Name)
+    }
+
+    $splat
+}
+
+Function Test-NewADUserParameters{
+    param(
+        $objActual,
+        $objExpected
+    )
+
+   
+    $allValid = $objActual |
+    ForEach-Object{
+        @{
+            Actual   = $($_)
+            Valid    = $($objExpected.Name -contains $_)
+        }
+    }
+
+    $allValid.Valid -notcontains $False
+}
+
+#region Arrange
+#Define proper dependencies
+#1) Verify xml file exist
+#   a) Verify Mandatory Name parameter
+#   b) Verify Valid parameters
+#2) Verify user can read AzureAD properties.
+
+
+$dependencies = @(
+    @{
+        Label  = 'AzureADPreview module is available '
+        Test   = {(Get-module -ListAvailable).Name -contains 'AzureADPreview'}
+        Action = {
+            #Import AzureADPreview
+            $null = Import-Module -Name AzureADPreview -Verbose:$false
+
+            #Get New-AzureADUser Parameters available
+            $script:parametersNewAzureADUser = (Get-Command New-AzureADUser -ErrorAction Stop).ParameterSets.Parameters | 
+            Where-Object {$_.IsDynamic -eq $true}
+        }
+    }
+
+    @{
+        Label  = "XML File at $($xmlFile) exists"
+        Test   = {Test-Path -Path $xmlFile}
+        Action = {
+            $script:xmlPSConfEU       = Import-Clixml -Path $xmlFile 
+            $script:xmlPSConfEUColumns = ($xmlPSConfEU | Get-Member -MemberType NoteProperty).Name
+            #Removing PasswordProfile from the equation as we can't verify this in hindsight
+            $script:UserProperties   = $xmlPSConfEUColumns.Where{$_ -ne 'PasswordProfile'}
+        }
+    }
+
+    @{
+        Label  = "XML contains Mandatory `'AccountEnabled`' parameter"
+        Test   = {$script:xmlPSConfEUColumns -Contains 'AccountEnabled' }
+        Action = {}
+    }
+
+    @{
+        Label  = "XML contains Mandatory `'DisplayName`' parameter"
+        Test   = {$script:xmlPSConfEUColumns -Contains 'DisplayName' }
+        Action = {}
+    }
+
+    @{
+        Label  = "XML contains Mandatory `'PasswordProfile`' parameter"
+        Test   = {$script:xmlPSConfEUColumns -Contains 'PasswordProfile' }
+        Action = {}
+    }
+
+    @{
+        Label  = "XML contains Mandatory `'MailNickName`' parameter"
+        Test   = {$script:xmlPSConfEUColumns -Contains 'MailNickName' }
+        Action = {}
+    }
+
+    @{
+        Label  = "XML contains valid parameters For cmdlet New-AzureADUser"
+        Test   = {Test-NewADUserParameters -objActual $script:xmlPSConfEUColumns -objExpected $parametersNewAzureADUser}
+        Action = {}
+    }
+
+    @{
+        Label  = "Current user can read AzureAD user object properties"
+        Test   = {[bool](Get-AzureADUser -ObjectId 632401f7-dfc9-42f8-948f-e149caa069e4)}
+        Action = {}
+    }
+)
+
+foreach($dependency in $dependencies){
+    if(!( & $dependency.Test)){
+        throw "The check: $($dependency.Label) failed. Halting script"
+    }
+    else{
+        Write-Host $($dependency.Label) -ForegroundColor Magenta
+        $dependency.Action.Invoke()
+    }
+}
+#endregion
+
+#region Main
+$xmlPSConfEU[3..6] |
+Foreach-Object{
+
+    Describe "Processing User $($Expected.DisplayName)"{
+
+        #region Assert
+        #1) Verify AD user has been created correctly
+        $Expected = $_
+
+        Context "Creating AzureAD User account for $($Expected.DisplayName) "{
+            #Convert to HashTable for splatting
+            $paramNewAzureADUser = ConvertTo-HashTable -PSObject $Expected
+
+            #region Act
+            #1) Create AZure ADUser from xml file
+
+            It "Created an account for $($Expected.DisplayName)"{
+                New-AzureADUser @paramNewAzureADUser
+            }
+            #endregion
+        }
+
+        Context "Verifying AD User properties for $($Expected.DisplayName)"{
+
+            #Get AzureAD user properties
+            $Actual = Get-AzureADUser -ObjectId $Expected.UserPrincipalName
+
+            foreach($parameter in $UserProperties){
+                It "Verified property $Parameter is set to $($Expected.$parameter) " {
+   
+                    $Actual.$parameter | should be $Expected.$parameter
+                }
+            }
+            #endregion
+        }
+    }
+}
+#endregion
---
+++ Irwin Strachan/Dependencies OVF/New-AzureRmADUser.ps1
@@ -0,0 +1,156 @@
+﻿#region Connect to AzureAD
+try{
+    Add-Type -AssemblyName Microsoft.Open.Azure.AD.CommonLibrary -ErrorAction Stop
+    Add-Type -AssemblyName Microsoft.Open.AzureAD16.Graph.Client    
+
+    $AzureCred = Import-Clixml -Path "${env:\userprofile}\AzureIrwin.Cred"  
+    $paramConnectAzureAD = @{
+        TenantID = $AzureCred.AzureIrwin.TenantID
+    }
+    Connect-AzureAD @paramConnectAzureAD -ErrorAction Stop
+}
+catch {
+    # get error record
+    [Management.Automation.ErrorRecord]$e = $_
+
+    # retrieve information about runtime error
+    $info = [PSCustomObject]@{
+        Exception = $e.Exception.Message
+        Reason    = $e.CategoryInfo.Reason
+        Target    = $e.CategoryInfo.TargetName
+        Script    = $e.InvocationInfo.ScriptName
+        Line      = $e.InvocationInfo.ScriptLineNumber
+        Column    = $e.InvocationInfo.OffsetInLine
+    }
+    
+    # output information. Post-process collected info, and log info (optional)
+    $info
+}
+#endregion 
+
+#region Get Parameters for New-AzureADUser
+$paramsNewAzureADUser = (Get-Command New-AzureADUser -ErrorAction Stop).ParameterSets.Parameters | 
+Where-Object {$_.IsDynamic -eq $true}
+Clear-Host
+'Total parameters New-AzureADUser cmdlet: {0}' -f $paramsNewAzureADUser.Count
+
+
+
+#Mandatory parameters New-AzureADUser
+$paramsNewAzureADUserMandatory = $paramsNewAzureADUser.Where{$_.IsMandatory -eq $true} |
+Select-Object -ExpandProperty Name
+Clear-Host
+'Mandatory New-AZureADUSer parameters: {0}' -f $($paramsNewAzureADUserMandatory -join ', ')
+#endregion
+
+#region Import CSV file
+$csvDemoUsers = Import-csv $PSScriptRoot\source\csv\PSConfEU.csv -Delimiter "`t" -Encoding UTF8 
+$csvDemoUsersHeaders = ($csvDemoUsers | Get-Member -MemberType NoteProperty).Name
+Clear-host
+Write-host 'CSV Headers' -ForegroundColor Magenta
+$csvDemoUsersHeaders
+#endregion
+
+#region Check if CSV has valid Headers
+Function Test-CSVNewAzureADUserParameters{
+    param(
+        $csvHeader,
+        $paramCmdlet
+    )
+
+    $csvHeader |
+    ForEach-Object{
+        [PSCustomObject]@{
+            Header   = $($_)
+            Valid    = $($paramCmdlet -contains $_)
+        }
+    }
+}
+
+$TestCSVHeaders = Test-CSVNewAzureADUserParameters -csvHeader $csvDemoUsersHeaders -paramCmdlet $paramsNewAzureADUser.Name
+$valid   = $TestCSVHeaders.Where{$_.valid -eq $true} | Select-Object -ExpandProperty Header
+Clear-host
+Write-Host 'Valid CSV headers for New-AzureADUser' -ForegroundColor Magenta
+$valid
+$invalid = $TestCSVHeaders.Where{$_.valid -eq $false} | Select-Object -ExpandProperty Header
+Clear-host
+Write-Host 'Invalid CSV headers for New-AzureADUser' -ForegroundColor Magenta
+$invalid
+#endregion
+
+#region Add PasswordProfile,AccountEnabled and MailNickName to 
+$newDemoUsers = $csvDemoUsers |
+ForEach-Object{
+    $obj = @{}
+    $item = $_ 
+
+    $item |
+    Get-Member -MemberType *Property |
+    ForEach-Object{
+        if( $Valid.Contains($_.Name)){
+            $obj.$($_.Name) = $item.$($_.Name)
+        }
+    }
+    
+    #Create PasswordProfile object
+    $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+    $PasswordProfile.Password = $_.Password
+    $PasswordProfile.EnforceChangePasswordPolicy = $true
+    $PasswordProfile.ForceChangePasswordNextLogin = $true
+
+    #Add Passwordprofile to hash
+    $obj.PasswordProfile =  $PasswordProfile
+    $obj.AccountEnabled = $true
+    #It seems that MailNickName is also Mandatory
+    $obj.MailNickName = ($obj.UserPrincipalName -split '@')[0]
+    [PSCustomObject]$obj
+}
+
+#region Save as XML file
+$newDemoUsers | Export-Clixml -Path $PSScriptRoot\source\xml\PSConfEU2018.xml -Encoding UTF8
+#endregion
+
+#Test Valid Headers again
+$DemoUsersHeaders = ($newDemoUsers | Get-Member -MemberType NoteProperty).Name
+$TestCSVHeaders = Test-CSVNewAzureADUserParameters -csvHeader $DemoUsersHeaders -paramCmdlet $paramsNewAzureADUser.Name
+Clear-host
+Write-Host 'Valid CSV headers for New-AzureADUser' -ForegroundColor Magenta
+$valid   = $TestCSVHeaders.Where{$_.valid -eq $true} | Select-Object -ExpandProperty Header
+$valid
+
+Clear-host
+Write-Host 'Invalid CSV headers for New-AzureADUser' -ForegroundColor Magenta
+$invalid = $TestCSVHeaders.Where{$_.valid -eq $false} | Select-Object -ExpandProperty Header
+$invalid
+#endregion
+
+#region Create New AzureAD User
+function ConvertTo-HashTable{
+    param(
+        [PSObject]$PSObject
+    )
+    $splat = @{}
+
+    $PSObject | 
+    Get-Member -MemberType *Property |
+    ForEach-Object{
+         $splat.$($_.Name) = $PSObject.$($_.Name)
+    }
+
+    $splat
+}
+
+$newAzureAdUser = ConvertTo-HashTable -PSObject $newDemoUsers[2]
+
+try{
+    New-AzureADUser @newAzureAdUser
+}
+catch [Microsoft.Open.AzureAD16.Client.ApiException]{
+    $WarningMessage = '{0} {1}' -f ($Error[0].Exception.Message -split "`n")[2], $newAzureAdUser.UserPrincipalName
+    Write-Warning -Message $WarningMessage
+}
+catch{
+    Write-Warning "Something went wrong"
+}
+#endregion
+
